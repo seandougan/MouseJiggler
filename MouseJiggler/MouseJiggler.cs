98a36cc79c14630b1ad8f8cd71c87091a07caaa2
sean <svoogan@hotmail.com>
2023-07-27 16:46:24 -0400

    UI jiggler

---
+++ MouseJiggler/MouseJiggler.cs
@@ -0,0 +1,26 @@
+﻿namespace MouseJiggler;
+
+using System.IO;
+
+public class MouseJiggler
+{
+    private double mousePositionX;
+    private double mousePositionY;
+    private bool isJiggling;
+
+    public MouseJiggler()
+    {
+        
+    }
+
+    public void JiggleMouse()
+    {
+        
+    }
+
+    public bool ToggleJiggle()
+    {
+        return true;
+    }
+
+}
