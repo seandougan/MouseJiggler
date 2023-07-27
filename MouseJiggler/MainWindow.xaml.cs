ee54f64367e7840691214a19553de4bf84adffd1
sean <svoogan@hotmail.com>
2023-07-27 18:27:08 -0400

    functional

--- MouseJiggler/MainWindow.xaml.cs
+++ MouseJiggler/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MouseJiggler;
 
 namespace MouseJiggler
 {
@@ -23,6 +24,36 @@ namespace MouseJiggler
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Jiggling");
+           await MouseJiggler();
+        } 
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Stop Jiggling");
+            return;
+        }
+
+        private async Task MouseJiggler()
+        {
+            int offsetX = 1;
+            int offsetY = 1;
+            Point currentPosition = Mouse.GetPosition(this); 
+            int newX = (int)currentPosition.X + offsetX;
+            int newY = (int)currentPosition.Y + offsetY;
+            
+            while (CheckBoxJiggle.IsChecked == true)
+            {
+               var cursorMoved = NativeMethods.SetCursorPosition(newX, newY);
+                await  Task.Delay(2500);
+                offsetX = offsetX * -1;
+                offsetY = offsetY * -1;
+                 newX = (int)currentPosition.X + offsetX;
+                 newY = (int)currentPosition.Y + offsetY;
+            }
         }
     }
 }
---
+++ MouseJiggler/NativeMethods.cs
@@ -0,0 +1,15 @@
+﻿namespace MouseJiggler;
+
+using System.Runtime.InteropServices;
+
+public static class NativeMethods
+{
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetCursorPos(int x, int y);
+
+    public static bool SetCursorPosition(int x, int y)
+    {
+        return SetCursorPos(x, y);
+    }
+}
